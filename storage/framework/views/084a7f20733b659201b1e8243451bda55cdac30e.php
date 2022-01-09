02cd56b003251a3aa3572662302990b20be7233a
Nisaaaesen <93565730+Nisaaaesen@users.noreply.github.com>
2022-01-09 21:28:40 +0700

    Add files via upload

---
+++ storage/framework/views/084a7f20733b659201b1e8243451bda55cdac30e.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Wather Heater</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="<?php echo e(asset('style/assets/favicon.ico')); ?>" />
+        <!-- Bootstrap Icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+        <!-- SimpleLightbox plugin CSS-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="<?php echo e(asset('style/css/styles.css')); ?>" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#page-top">Waterheater</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="#about">Tentang</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#services">List Harga</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#portfolio">Gallery</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#contact">Contact Person</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container px-4 px-lg-5 h-100">
+                <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-8 align-self-end">
+                        <h1 class="text-white font-weight-bold">Selamat Datang Di Website Gudang WaterHeater</h1>
+                        <hr class="divider" />
+                    </div>
+                    <div class="col-lg-8 align-self-baseline">
+                        <p class="text-white-75 mb-5">Apapun Kebutuhan Air Panas Anda Percayakan Pada Kami, Ahlinnya Pemanas Air</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- About-->
+        <section class="page-section bg-primary" id="about">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h2 class="text-white mt-0">Apa Itu Waterheater ??</h2>
+                        <hr class="divider divider-light" />
+                        <p class="text-white-75 mb-4"> Water heater adalah alat yang digunakan untuk memanaskan atau menghangatkan air. Dengan menggunakan water heater kamu tidak perlu lagi memasak air untuk mandi atau memenuhi kebutuhan sehari-hari. 
+                            Gudang waterheater telah dipercaya oleh ribuan keluarga karena gudang waterheater memberikan solusi terlengkap bagi kebutuhan air panas anda dan juga  selalu mengedepankan kenyamanan dan kepuasan pelanggan. </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Services-->
+        <section class="page-section" id="services">
+            <div class="container px-4 px-lg-5">
+                <h2 class="text-center mt-0">Daftar Harga</h2>
+                <hr class="divider" />
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"></div>
+                            <h3 class="h4 mb-2">WaterHeater Digital Kaca</h3>
+                            <p class="text-muted mb-0">Cash : Rp.2.100.000</p>
+                            <p class="text-muted mb-0">kredit : Rp.799.000 3x Angsuran</p>
+                            <p class="text-muted mb-0">kredit : Rp.375.000 7x Angsuran</p>
+                            <p class="text-muted mb-0">kredit : Rp.275.000 11x Angsuran</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"></div>
+                            <h3 class="h4 mb-2">WaterHeater Digital Standar</h3>
+                            <p class="text-muted mb-0">Cash : Rp.1.950.000</p>
+                            <p class="text-muted mb-0">kredit : Rp.750.000 3x Angsuran</p>
+                            <p class="text-muted mb-0">kredit : Rp.349.000 7x Angsuran</p>
+                            <p class="text-muted mb-0">kredit : Rp.249.000 11x Angsuran</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"></i></div>
+                            <h3 class="h4 mb-2">WaterHeater Listrik 15 Liter</h3>
+                            <p class="text-muted mb-0">Cash : Rp.3.000.000</p>
+                            <p class="text-muted mb-0">kredit : Rp.399.000 11x Angsuran</p>
+                            <p class="text-muted mb-0">kredit : Rp.500.000 7x Angsuran</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"></i></div>
+                            <h3 class="h4 mb-2">WaterHeater + Pompa</h3>
+                            <p class="text-muted mb-0">Pompa Non Otomatis : Rp.650.000</p>
+                            <p class="text-muted mb-0">Pompa Otomatis : Rp.850.000 </p>
+                            <p class="text-muted mb-0">Keran Mixer Panas : Rp.450.000 </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Portfolio-->
+        <div id="portfolio">
+            <h1 align="center"> Gallery </h1>
+            <div class="container-fluid p-0">
+                <div class="row g-0">
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="<?php echo e(asset('style/assets/img/portfolio/fullsize/img6.jpg')); ?>" title="Project Name">
+                            <img class="img-fluid" src="<?php echo e(asset('style/assets/img/portfolio/thumbnails/img6.jpg')); ?>" alt="..." />
+                            <div class="portfolio-box-caption">
+                                
+                                <div class="project-name">Project Name</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="<?php echo e(asset('style/assets/img/portfolio/fullsize/img2.jpg')); ?>" title="Project Name">
+                            <img class="img-fluid" src="<?php echo e(asset('style/assets/img/portfolio/thumbnails/img2.jpg')); ?>" alt="..." />
+                            <div class="portfolio-box-caption">
+                                
+                                <div class="project-name">Project Name</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="<?php echo e(asset('style/assets/img/portfolio/fullsize/img3.jpg')); ?>" title="Project Name">
+                            <img class="img-fluid" src="<?php echo e(asset('style/assets/img/portfolio/thumbnails/img3.jpg')); ?>" alt="..." />
+                            <div class="portfolio-box-caption">
+                               
+                                <div class="project-name">Project Name</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="<?php echo e(asset('style/assets/img/portfolio/fullsize/img4.jpg')); ?>" title="Project Name">
+                            <img class="img-fluid" src="<?php echo e(asset('style/assets/img/portfolio/thumbnails/img4.jpg')); ?>" alt="..." />
+                            <div class="portfolio-box-caption">
+                                
+                                <div class="project-name">Project Name</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="<?php echo e(asset('style/assets/img/portfolio/fullsize/img5.jpg')); ?>" title="Project Name">
+                            <img class="img-fluid" src="<?php echo e(asset('style/assets/img/portfolio/thumbnails/img5.jpg')); ?>" alt="..." />
+                            <div class="portfolio-box-caption">
+                                
+                                <div class="project-name">Project Name</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="<?php echo e(asset('style/assets/img/portfolio/fullsize/img1.jpeg')); ?>" title="Project Name">
+                            <img class="img-fluid" src="<?php echo e(asset('style/assets/img/portfolio/thumbnails/img1.jpeg')); ?>" alt="..." />
+                            <div class="portfolio-box-caption p-3">
+                                
+                                <div class="project-name">Project Name</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Call to action-->
+        <!-- Contact-->
+        <section class="page-section" id="contact">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8 col-xl-6 text-center">
+                        <h2 ="mt-0">Minat?? Segera Hubungi Kami !!!</h2>
+                        <hr class="divider" />
+                    </div>
+                </div>
+                <div class="row gx-4 gx-lg-5 justify-content-center mb-5">
+                    <div class="col-lg-6">
+                        <!-- * * * * * * * * * * * * * * *-->
+                        <!-- * * SB Forms Contact Form * *-->
+                        <!-- * * * * * * * * * * * * * * *-->
+                        <!-- This form is pre-integrated with SB Forms.-->
+                        <!-- To make this form functional, sign up at-->
+                        <!-- https://startbootstrap.com/solution/contact-forms-->
+                        <!-- to get an API token!-->
+                       
+                    </div>
+                </div>
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-4 text-center mb-5 mb-lg-0">
+                        <i class="bi-whatsapp fs-2 mb-3 text-muted"></i>
+                        <p> WhatsApp </p>
+                        <div> +6281312941956
+                        </div>
+                    </div>
+                </div>
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-4 text-center mb-5 mb-lg-0">
+                        <i class="bi-instagram fs-2 mb-3 text-muted"></i>
+                        <p> Instagram </p>
+                        <div><i>@gudangwaterheater</div>
+                    </div>
+                </div>
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-4 text-center mb-5 mb-lg-0">
+                        <i class="bi-phone fs-2 mb-3 text-muted"></i>
+                        <p>No Hp </p>
+                        <div>+628239803842 </div>
+                    </div>
+                </div>
+            </div>
+            <div align="center">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15847.312531761605!2d107.9399169!3d-6.7907571!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9882e5d7a60c1dc7!2sKantor%20Gudang%20Waterheater!5e0!3m2!1sid!2sid!4v1640431482199!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="bg-light py-5">
+            <div class="container px-4 px-lg-5"><div class="small text-center text-muted">Copyright &copy; 2022 - Nisa Siti Nurlaela</div></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- SimpleLightbox plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
+<?php /**PATH C:\xampp\htdocs\project-nisa\resources\views/main.blade.php ENDPATH**/ ?>
